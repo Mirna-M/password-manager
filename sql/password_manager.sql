e098f1a24d4a8db01f1aab6b30c7f0b61d5031ec
Mirna-M <mirna.martinovic1@gmail.com>
2013-10-11 08:56:17 +0200

    Fix incorrect names for two columns

--- sql/password_manager.sql
+++ sql/password_manager.sql
@@ -5,15 +5,16 @@ USE password_manager;
 CREATE TABLE users (
     user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_login VARCHAR(50) NOT NULL,
-    user_pass TEXT NOT NULL,
+    user_pass CHAR(32) NOT NULL,
     PRIMARY KEY(user_id)
 ) DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 
 CREATE TABLE user_data (
     user_data_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_data_user id INT UNSIGNED NOT NULL,
     user_data_for VARCHAR(50) NOT NULL,
-    user_name VARCHAR(50) NOT NULL,
-    user_password TEXT NOT NULL,
+    user_data_name VARCHAR(50) NOT NULL,
+    user_data_password TEXT NOT NULL,
     PRIMARY KEY(user_data_id),
     FOREIGN KEY(user_data_user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
